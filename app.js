9b8e273ee8262cf0e983e9c2538adb64fc9573b1
YiroYi <yiroyi@live.com>
2021-04-11 18:55:38 -0500

    Add: Handling errors main folder

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+dotenv.config();
+
+const mongoDbUrl = process.env.DB_URL
+const MONGODB_URI = mongoDbUrl;
+
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,4 +25,18 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes)
 
-app.listen(3000);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({message: message});
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {console.log(err)})
+
+
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator/check');
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -21,26 +22,36 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if(!errors.isEmpty()) {
-    return res.status(422).json(
-      {
-        message: 'Validation failed',
-        errors: errors.array()
-      }
-    );
+    const error = new Error('Validation failed');
+    error.statusCode = 422
+    throw error;
   }
-  const title = req.body.title;
-  const content = req.body.title;
 
-  res.status(201).json({
-    message: 'Post created succefully',
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: 'Yiro Yi'
-      },
-      createdAt: new Date()
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/cat.png'
+    creator: {
+      name: 'Yiro Yi'
     }
   });
+
+  post
+    .save()
+    .then(result => {
+      const title = req.body.title;
+      const content = req.body.title;
+
+      res.status(201).json({
+        message: 'Post created succefully',
+        post: result
+      });
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
+    });
 }
