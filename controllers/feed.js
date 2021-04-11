5ba7a7b47cb0175e0f5b958305e0e93e08ca5597
YiroYi <yiroyi@live.com>
2021-04-11 16:53:07 -0500

    Add: routing to the project

---
+++ controllers/feed.js
@@ -0,0 +1,30 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: 1,
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/cat.png',
+        creator: {
+          name: 'Yiro Yi'
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.title;
+
+  res.status(201).json({
+    message: 'Post created succefully',
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content
+    }
+  });
+}
