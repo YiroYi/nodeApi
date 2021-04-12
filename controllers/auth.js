70ba67c7e6e97926d5e9674c035ce598821a5653
YiroYi <yiroyi@live.com>
2021-04-11 21:29:06 -0500

    Add: JWT token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -37,4 +38,46 @@ exports.signup = (req, res, next) => {
 
           next(err);
         })
+}
+
+exports.login = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  User.findOne({email: email})
+      .then(user => {
+        if(!user) {
+          const error = new Error('User does not exist');
+          error.statusCode = 401;
+          throw error;
+        }
+
+        loadedUser = user;
+
+        return bcrypt.compare(password, user.password)
+      })
+      .then(isEqual => {
+        if(!isEqual) {
+          const error = new Error('Wrong password');
+          error.statusCode = 401;
+          throw error;
+        }
+
+        const token = jwt.sign({
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'secret',
+        {expiresIn: '1h'});
+
+        res.status(200).json({token: token, userId: loadedUser._id.toString()})
+      })
+      .catch(err => {
+          if(!err.statusCode) {
+            err.statusCode = 500;
+          }
+
+          next(err);
+        })
+
 }
