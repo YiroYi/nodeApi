0086a5928584974cf00ecad147cb8337d1706ebf
YiroYi <yiroyi@live.com>
2021-04-11 19:31:18 -0500

    Add: multer to upload images

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/png' ||
+     file.mimetype === 'image/jpg' ||
+     file.mimetype === 'image/jpeg') {
+
+    cb(null, true);
+  } else {
+
+    cb(null, false);
+  }
+}
 
 dotenv.config();
 
@@ -14,6 +36,7 @@ const mongoDbUrl = process.env.DB_URL
 const MONGODB_URI = mongoDbUrl;
 
 app.use(bodyParser.json());
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -2,23 +2,22 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1,
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/cat.png',
-        creator: {
-          name: 'Yiro Yi'
-        },
-        createdAt: new Date()
+  Post.find()
+      .then(posts => {
+        res.status(200).json({message: 'Fetched succesfully', posts: posts});
+      })
+      .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
       }
-    ]
-  });
+
+      next(err);
+      });
 };
 
 exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.title;
   const errors = validationResult(req);
 
   if(!errors.isEmpty()) {
@@ -27,20 +26,25 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if(!req.file) {
+    const error = new Error('No attached file');
+    error.statusCode = 422
+    throw error;
+  }
+
+  const imageUrl = req.file.path;
+
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/cat.png'
+    imageUrl: imageUrl,
     creator: {
       name: 'Yiro Yi'
     }
   });
 
-  post
-    .save()
+  post.save()
     .then(result => {
-      const title = req.body.title;
-      const content = req.body.title;
 
       res.status(201).json({
         message: 'Post created succefully',
@@ -48,6 +52,28 @@ exports.createPost = (req, res, next) => {
       });
     })
     .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
+    });
+}
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+      .then(post => {
+        if(!post) {
+          const error = new Error('Could not find post.');
+          error.statusCode = 404;
+          throw error;
+        }
+
+        res.status(200).json({message: 'Post fetched', post: post});
+      })
+      .catch(err => {
       if(!err.statusCode) {
         err.statusCode = 500;
       }
